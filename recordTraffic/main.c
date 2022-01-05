e1f24a4e0a34fedb0f5507e9bf7991345215bb0e
Gesch <gesc_jo@rmc-070402dl.intra.dlr.de>
2022-01-05 18:24:43 +0100

    Add function for detecting Link Analyzer devices
    The function is scanning for devices of the type
    STAR_DEVICE_LINK_ANALYSER_MK3 and stores the last found device
    in a variable.

--- recordTraffic/main.c
+++ recordTraffic/main.c
@@ -90,6 +90,8 @@ void firmwareVersionExample()
     unsigned int index;
     /* Get device list */
     STAR_DEVICE_ID *devices = STAR_getDeviceList(&deviceCount);
+    printf("Detected %d devices\n", deviceCount);
+
 
     if (deviceCount)
     {
@@ -107,19 +109,63 @@ void firmwareVersionExample()
     }
     else
     {
-        printf("No devices detected!\n");
+        //printf("No devices detected!\n");
     }
     /* Destroy device list */
     STAR_destroyDeviceList(devices);
 }
 
+void detectLinkAnalyser(STAR_LA_LinkAnalyser *linkAnalyser)
+{
+    /* Initialise device count to 0 */
+    U32 deviceCount = 0;
+    /* Counter for loop */
+    unsigned int index;
+    /* Get list of Link Analyser Mk3 devices */
+    STAR_DEVICE_ID *devices = STAR_getDeviceListForTypes(&(linkAnalyser->linkAnalyserType), 1, &deviceCount);
+    printf("Detected %d device(s) of 'Type STAR_DEVICE_LINK_ANALYSER_MK3'\n", deviceCount);
+
+
+    if (deviceCount)
+    {
+        /* For all devices */
+        for(index = 0; (devices != NULL) && (index < deviceCount); index++)
+        {
+            /* Print device name and serial number */
+            printf("Device Name: %s\n", STAR_getDeviceName(devices[index]));
+            printf("Serial Number: %s\n", STAR_getDeviceSerialNumber(devices[index]));
+            /* Get firmware version */
+            STAR_VERSION_INFO * firmwareVersion = STAR_getDeviceFirmwareVersion(
+                devices[index]);
+            /* Print firmware version label */
+            printf("Firmware version:");
+            /* Print firmware version */
+            printVersionInfo(firmwareVersion);
+
+            /** Store deviceID
+            /* \todo check for specific serial number
+             */
+            linkAnalyser->deviceID = devices[index];
+        }
+    }
+    else
+    {
+        linkAnalyser->deviceID = STAR_DEVICE_UNKNOWN;
+    }
+
+    /* Destroy device list */
+    STAR_destroyDeviceList(devices);
+}
+
+
 int main()
 {
     STAR_LA_LinkAnalyser linkAnalyser;
+    linkAnalyser.linkAnalyserType = STAR_DEVICE_LINK_ANALYSER_MK3;
 
     print_api_version();
 
-    firmwareVersionExample();
+    detectLinkAnalyser(&linkAnalyser);
 
     //get_all_recorded_traffic_mk3(linkAnalyser);
 
