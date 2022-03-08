431d347228f8637890a33d6aba0cb7f261d9940c
Jonas Gesch <jonas.gesch@dlr.de>
2022-03-08 20:51:07 +0100

    Change dissector formatting

--- src/plato.lua
+++ src/plato.lua
@@ -1,5 +1,14 @@
--- Dissector for PLATO Proprietary Protocol (PPP)
--- as defined in PLATO-DLR-PL-ICD-0007 Rev 2.0 chapter 5
+---------------------------------------------------------------
+-- @file plato.lua
+-- @author Jonas Gesch (jonas.gesch@dlr.de)
+-- @brief Dissector for PLATO Proprietary Protocol (PPP)
+--        as defined in PLATO-DLR-PL-ICD-0007 Rev 2.0 chapter 5
+-- @date 2022-02-23
+---------------------------------------------------------------
+
+local CCD_SIDE_E = 0
+local CCD_SIDE_F = 1
+local HEADER_LENGTH = 12
 
 plato_protocol = Proto("PLATO",  "PLATO Proprietary Protocol")
 
@@ -29,28 +38,30 @@ function plato_protocol.dissector(buffer, pinfo, tree)
 
   -- Type field (2 byte, bits 15-11 and 3-2 reserved for future use)
   local type_field = buffer(4,2):int()
-  local type_field_string = string.format("Type: 0x%04x", type_field)  
-  
+  local type_field_string = string.format("Type: 0x%04x", type_field)
+
   -- Right shift and mask relevant bits to add information as text
   local deb_mode_number = bit.band(bit.rshift(type_field, 8), 0x0007)
   local deb_mode_string = get_deb_mode_name(deb_mode_number)
-  
+
+  local aeb_id_number = bit.band(bit.rshift(type_field, 4), 0x0003) + 1
+
   local ccd_side_flag = bit.band(bit.rshift(type_field, 6), 0x0001)
   local ccd_side_string = get_ccd_side(ccd_side_flag)
-  
+
   local packet_type_number = bit.band(type_field, 0x0003)
   local packet_type_string = get_packet_type_name(packet_type_number)
 
   -- Info for Wireshark columns
   pinfo.cols.protocol = plato_protocol.name
-  info_string = string.format("TargetAddr = 0x%02x     Length = %3d     Type = %s", buffer(0,1):int(), msg_length, packet_type_string) 
+  info_string = string.format("Length = %3d     Type = %15s     CCD = %d%s     SeqCounter = %d", msg_length, packet_type_string, aeb_id_number, ccd_side_string, buffer(8,2):int())
   pinfo.cols.info = info_string
 
   -- PLATO subtree
   local subtree = tree:add(plato_protocol, buffer(), "PLATO Protocol Data")
 
 
-  -- Header subtree  
+  -- Header subtree
   local headerSubtree = subtree:add(plato_protocol, buffer(), "Header")
   headerSubtree:add(target_address, buffer(0,1))
   headerSubtree:add(protocol_id, buffer(1,1))
@@ -64,17 +75,17 @@ function plato_protocol.dissector(buffer, pinfo, tree)
 	typeSubtree:add(packet_type, buffer(4,2)):append_text(" (" .. packet_type_string .. ")")
   headerSubtree:add(frame_counter, buffer(6,2))
   headerSubtree:add(seq_counter, buffer(8,2))
-  --headerSubtree:add(reserved, buffer(10,1))  
+  --headerSubtree:add(reserved, buffer(10,1))
   headerSubtree:add(header_crc, buffer(11,1))
-  
-  
+
+
   -- Payload subtree
-  if length > 12 then    
+  if length > HEADER_LENGTH then
     local payloadSubtree = subtree:add(plato_protocol, buffer(), "Payload")
-	payloadSubtree:add(data_field, buffer(12, msg_length))
-	payloadSubtree:add(data_crc, buffer(12 + msg_length, 1))
+	payloadSubtree:add(data_field, buffer(HEADER_LENGTH, msg_length - 1))
+	payloadSubtree:add(data_crc, buffer(HEADER_LENGTH + msg_length - 1, 1))
   end
-  
+
 end
 
 -- DEB mode text information
@@ -93,8 +104,8 @@ end
 function get_ccd_side(ccd_side_flag)
   local ccd_side = "Unknown"
 
-      if ccd_side_flag == 0 then ccd_side = "Left/E"
-  elseif ccd_side_flag == 1 then ccd_side = "Right/F" end
+      if ccd_side_flag == CCD_SIDE_E then ccd_side = 'E'
+  elseif ccd_side_flag == CCD_SIDE_F then ccd_side = 'F' end
 
   return ccd_side
 end
@@ -103,10 +114,10 @@ end
 function get_packet_type_name(packet_type)
   local packet_type_name = "Unknown"
 
-      if packet_type == 0 then packet_type_name = "Data packet"
-  elseif packet_type == 1 then packet_type_name = "Overscan data packet"
-  elseif packet_type == 2 then packet_type_name = "DEB houskeeping packet"
-  elseif packet_type == 3 then packet_type_name = "AEB houskeeping packet" end
+      if packet_type == 0 then packet_type_name = "Data"
+  elseif packet_type == 1 then packet_type_name = "Overscan data"
+  elseif packet_type == 2 then packet_type_name = "DEB houskeeping"
+  elseif packet_type == 3 then packet_type_name = "AEB houskeeping" end
 
   return packet_type_name
 end
