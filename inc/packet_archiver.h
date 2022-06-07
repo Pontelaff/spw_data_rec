0c49325805b26775e1b1571b93c62f5ea33dd636
Jonas Gesch <jonas.gesch@dlr.de>
2022-06-07 17:50:55 +0200

    Fix typos

--- inc/packet_archiver.h
+++ inc/packet_archiver.h
@@ -7,7 +7,7 @@
  * @date 2022-04-27
  */
 
-#include <rdkafka.h>
+#include <librdkafka/rdkafka.h>
 #include <spw_la_api.h>
 
 #define BUF_SIZE 1000050
@@ -17,20 +17,20 @@ typedef struct settings Settings;
 typedef struct packetInfo
 {
 	uint8_t *captureTime;	/* Packet timestamp */
-	uint8_t *interfaceId;	/* Receiver on wich the packet was received */
+	uint8_t *interfaceId;	/* Receiver on which the packet was received */
 	char 	*rawData;    	/* String of raw packet data */
 	size_t 	rawDataLength;	/* Size of raw data string */
 } PacketInfo;
 
 /**
  * @brief This function creates data packets from recorded events and archives them in the database as individual kafka messages.
- * 
+ *
  * @param settings The settings of this application containing static information to be sent via kafka.
  * @param pTraffic The recorded events.
  * @param trafficCount The amount of recorded events.
  * @param charCaptureClockPeriod The character capture clock period.
  * @param triggerTime The timestamp at which the Link Analyser was triggered.
  * @param preTrigger The maximum duration for which packets received BEFORE the trigger are archived.
- * @return A non-zeor value on success. 
+ * @return A non-zero value on success.
  */
 int32_t LA_MK3_archiveCapturedPackets(Settings settings, STAR_LA_MK3_Traffic *pTraffic, const U32 *trafficCount, const double *charCaptureClockPeriod, struct timespec *triggerTime, const int preTrigger);
